669fb4bef59fa5c51d4d2396296eea9a9455ff59
Romain Benard <romain.benard@swile.co>
2023-08-03 18:49:47 +0200

    chore: basic ts x express setup

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+
+dotenv.config()
+
+const port = process.env.PORT
+
+const app = express()
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})

bca57b3da5c3eccc80885469b6c067aa5c2445b3
Romain Benard <romain.benard@swile.co>
2023-08-21 10:05:23 +0200

    feat: add strong validation on password

--- src/validations/users.validation.ts
+++ src/validations/users.validation.ts
@@ -1,9 +1,14 @@
 import { z } from 'zod'
 
+const passwordRegex = new RegExp(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{8,}$/,
+  'g'
+)
+
 export const createUserValidation = z.object({
   email: z.string().email(),
   name: z.string(),
-  password: z.string(),
+  password: z.string().regex(passwordRegex),
 })
 
 export const updateUserValidation = createUserValidation.partial()
